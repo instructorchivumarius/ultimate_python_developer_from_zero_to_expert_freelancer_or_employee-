c66a4b1862c8345dc50b4136c04767b0efdc1355
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-11 12:25:58 +0200

    If - Else Statements

---
+++ fundamentals/flow_cntrol/if_else.py
@@ -0,0 +1,19 @@
+# If – Else in Python 
+
+# ───────────────────────────────
+# (1) BASIC IF – ELSE
+number = -5
+if number >= 0:
+    print("The number is positive")
+else:
+    print("The number is negative")
+
+print("-" * 50)
+
+# ───────────────────────────────
+# (2) IF – ELSE WITH STRING
+user = "guest"
+if user == "admin":
+    print("Welcome, Admin!")
+else:
+    print("Access limited for guests")
