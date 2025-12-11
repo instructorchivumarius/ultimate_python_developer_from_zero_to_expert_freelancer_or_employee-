567b5f8ec4f02bd802e164fb332682ad892e79e4
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-11 12:57:26 +0200

    Defining and Calling Simple Functions

---
+++ fundamentals/functions/simple_functions.py
@@ -0,0 +1,15 @@
+# Defining and Calling Simple Functions in Python 
+
+# (1) SIMPLE FUNCTION WITHOUT PARAMETERS
+def greet():
+    print("Hello, welcome to Python Functions!")
+
+# calling the function
+greet()
+
+# (2) ANOTHER SIMPLE FUNCTION
+def show_message():
+    print("Python is fun!")
+
+# calling the function
+show_message()
