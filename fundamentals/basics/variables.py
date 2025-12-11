c835ddb2ab04997a619bb05db7940a4810fefa9b
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-11 11:26:20 +0200

    Variables

---
+++ fundamentals/basics/variables.py
@@ -0,0 +1,31 @@
+# Declaring a variable
+name = "Andrew"
+age = 25
+price = 19.99
+is_student = True
+
+# Printing the values
+print("Name:", name)
+print("Age:", age)
+print("Price:", price)
+print("Is student?", is_student)
+
+# Reassigning a variable
+age = 26
+print("Updated age:", age)
+
+# Valid variable names
+full_name = "Maria Popescu"
+x = 5
+_y = 10
+PI = 3.14
+
+# Invalid names (DO NOT run these!)
+# 2name = "Anna"
+# full name = "John Smith"
+
+# Implicit and converted types
+x = 7       # int
+y = "7"     # str
+z = float(x)
+print("z is float:", z)
