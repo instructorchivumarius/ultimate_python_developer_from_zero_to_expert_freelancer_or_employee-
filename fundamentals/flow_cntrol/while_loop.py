0277e1ae746d77baa083007ed97c83bbe39be69d
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-11 12:35:53 +0200

    While Loop

---
+++ fundamentals/flow_cntrol/while_loop.py
@@ -0,0 +1,17 @@
+# While Loops in Python 
+
+# ───────────────────────────────
+# (1) BASIC WHILE LOOP
+count = 1
+while count <= 3:
+    print("Count:", count)
+    count += 1
+
+print("-" * 50)
+
+# ───────────────────────────────
+# (2) WHILE LOOP with CONDITION
+number = 5
+while number > 0:
+    print("Number is:", number)
+    number -= 2
