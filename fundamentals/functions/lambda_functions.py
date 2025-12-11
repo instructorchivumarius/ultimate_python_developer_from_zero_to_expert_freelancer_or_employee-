2184752e0dc10565fbe93fd98e535f6d938984c8
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-11 15:12:27 +0200

    Lambda (Anonymous) Functions

---
+++ fundamentals/functions/lambda_functions.py
@@ -0,0 +1,9 @@
+# Lambda (Anonymous) Functions in Python 
+
+# (1) SIMPLE LAMBDA FUNCTION
+square = lambda x: x * x
+print("Square of 4:", square(4))
+
+# (2) LAMBDA FUNCTION WITH TWO PARAMETERS
+add = lambda a, b: a + b
+print("Sum of 3 and 5:", add(3, 5))
