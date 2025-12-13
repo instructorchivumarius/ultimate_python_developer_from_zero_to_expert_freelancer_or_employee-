11f15e90a708ad18a7a196cb06e20966338bc3ca
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-13 10:34:31 +0200

    Using super() to Call Parent Method

--- intermediate/oop/inheritance.py
+++ intermediate/oop/inheritance.py
@@ -7,6 +7,10 @@
 #     We continue in the same file inheritance.py from the previous lesson.
 #     In this lesson, we learn how child classes can override parent methods
 #     to provide their own specific behavior.
+#
+# [3] Using “super()” Method
+#     In this lesson, we learn how to use super()
+#     to call the parent’s version of a method before or after customizing it.
 #____________________________________________________________________________________________
 
 
@@ -23,10 +27,18 @@ class Dog(Animal):
     # [1]-2-1 METHOD IN DOG CLASS
     def bark(self):
         print("The dog barks: Woof!")
+ 
 
     # [2]-1 OVERRIDE METHOD speak()
     def speak(self):
         print("The dog says: Woof!")
+   
+
+    # [3]-1 USE super() TO CALL PARENT METHOD
+    def show_behavior(self):
+        print("Dog behavior:")
+        super().speak()   # Calls Animal.speak()
+        print("Then, the dog barks happily.")
 
 
 
@@ -36,17 +48,29 @@ class Cat(Animal):
     def meow(self):
         print("The cat meows: Meow!")
 
+
     # [2]-2 OVERRIDE METHOD speak()
     def speak(self):
         print("The cat says: Meow!")
 
 
+    # [3]-2 USE super() TO CALL PARENT METHOD
+    def show_behavior(self):
+        print("Cat behavior:")
+        super().speak()   # Calls Animal.speak()
+        print("Then, the cat purrs softly.")
 
-# [1]-4 CREATE OBJECTS AND USE METHODS (from previous lesson)
+
+
+# [1]-4 CREATE OBJECTS AND USE METHODS
 dog = Dog()
-dog.speak()   # Inherited method (now overridden)
+dog.speak()   # Inherited method
 # dog.bark()    # Specific to Dog
 
 cat = Cat()
-cat.speak()   # Inherited method (now overridden)
+cat.speak()   # Inherited method
 # cat.meow()    # Specific to Cat
+
+# [3]-3 CALL METHODS THAT USE “super()”
+dog.show_behavior()
+cat.show_behavior()
