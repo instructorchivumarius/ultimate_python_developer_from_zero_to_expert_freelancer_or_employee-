4095540dd23c2abf5a61329f81b6750ec3b60284
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-13 10:38:11 +0200

    Polymorphism in Methods and Functions

--- intermediate/oop/polymorphism.py
+++ intermediate/oop/polymorphism.py
@@ -3,6 +3,11 @@
 #     can share the same method name but behave differently.
 #     Polymorphism allows us to use a common interface
 #     while keeping unique behaviors in each subclass.
+#
+# [2] Polymorphism in Functions
+#     We continue in the same file polymorphism.py from the previous lesson.
+#     In this lesson, we use functions that work with multiple object types.
+#     Any object that implements draw() can be used with the same function.
 #____________________________________________________________________________________________
 
 
@@ -13,13 +18,11 @@ class Shape:
         print("Drawing a generic shape.")
 
 
-
 # [1]-2 DEFINE CHILD CLASS CIRCLE
 class Circle(Shape):
     # [1]-2-1 OVERRIDE METHOD draw()
     def draw(self):
         print("Drawing a circle.")
-
 
 
 # [1]-3 DEFINE CHILD CLASS SQUARE
@@ -29,7 +32,6 @@ class Square(Shape):
         print("Drawing a square.")
 
 
-
 # [1]-4 DEFINE CHILD CLASS TRIANGLE
 class Triangle(Shape):
     # [1]-4-1 OVERRIDE METHOD draw()
@@ -37,16 +39,32 @@ class Triangle(Shape):
         print("Drawing a triangle.")
 
 
-
-# [1]-5 CREATE OBJECTS
-c = Circle()
-s = Square()
-t = Triangle()
-
+# [1]-5 CREATE OBJECTS (FROM PREVIOUS LESSON)
+# c = Circle()
+# s = Square()
+# t = Triangle()
 
 
-# [1]-6 USE POLYMORPHISM (SAME INTERFACE, DIFFERENT BEHAVIOR)
-shapes = [c, s, t]  # list of Shape references
+# [1]-6 USE POLYMORPHISM (COMMENTED, FROM PREVIOUS LESSON)
+# shapes = [c, s, t]  # list of Shape references
+# for sh in shapes:
+#     sh.draw()       # each object responds with its own draw()
 
-for sh in shapes:
-    sh.draw()       # each object responds with its own draw()
+
+# [2]-1 DEFINE A FUNCTION THAT USES POLYMORPHISM
+def render_shape(shape):
+    print("Rendering shape:")
+    shape.draw()      # Works for any object with draw()
+    print()
+
+
+# [2]-2 CREATE OBJECTS
+circle = Circle()
+square = Square()
+triangle = Triangle()
+
+
+# [2]-3 CALL FUNCTION WITH DIFFERENT OBJECTS
+render_shape(circle)
+render_shape(square)
+render_shape(triangle)
