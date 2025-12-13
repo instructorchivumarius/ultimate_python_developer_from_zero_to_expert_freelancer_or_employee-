61c8938d8cdca2da3f23fc17335ccbf598e469f8
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-13 10:56:03 +0200

    What Is a Module?

---
+++ intermediate/modules_and_packages/module.py
@@ -0,0 +1,9 @@
+# What Is a Module?
+# In this lesson we learn what a Python module is.
+
+# (1) IMPORTING A BUILT-IN MODULE
+import math
+
+# (2) USING FUNCTIONS FROM THE MODULE
+print(math.sqrt(16))   # Square root
+print(math.pi)         # Pi constant
