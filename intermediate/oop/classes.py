e258543e7fce8a6a6b7b4a8a365885a0bc9346c2
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-13 09:55:40 +0200

    Classes

---
+++ intermediate/oop/classes.py
@@ -0,0 +1,11 @@
+# Classes
+
+# (1) DEFINE AN EMPTY CLASS
+class EmptyClass:
+    pass
+
+
+# (2) DEFINE A CLASS WITH VARIABLES (characteristics only)
+class Car:
+    brand = "Tesla"
+    color = "Red"
