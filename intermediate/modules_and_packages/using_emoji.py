b830bb75c243b353c0a25d76d17de819bce345ab
instructorchivumarius <instructor.chivumarius@gmail.com>
2025-12-15 12:24:37 +0200

    PyPI.org Platform & Third-Party Packages

---
+++ intermediate/modules_and_packages/using_emoji.py
@@ -0,0 +1,7 @@
+# PyPI & Third-party Packages
+# Example with emoji package
+
+import emoji
+
+print(emoji.emojize("Python is fun :snake: :rocket:"))
+print(emoji.emojize("I love coding :red_heart:"))
